8769f84118fda84fa3e2205360056285deb06e7e
Wu Jane <jane72418@gmail.com>
2023-12-14 20:30:49 +0800

    deploy

---
+++ assets/alertMixin-36029470.js
@@ -0,0 +1,1 @@
+import{S as e}from"./toastMixin-2ca399d5.js";const r={methods:{showAlert(t){return e.fire({confirmButtonText:"確定",cancelButtonText:"取消",...t})}}};export{r as a};
---
+++ assets/icons8-book4-80bb2ce7.js
@@ -0,0 +1,1 @@
+const s={data(){return{isLoading:!1}},methods:{startLoading(){this.isLoading=!0},stopLoading(){this.isLoading=!1}}},o="/RECA-Bookstore/assets/icons8-book4-7bf5cafd.gif";export{o as _,s as l};
