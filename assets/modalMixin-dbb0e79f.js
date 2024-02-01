3b8b0abfe6208e39ff7b2d8ce1d2f98211ebff48
Wu Jane <jane72418@gmail.com>
2024-02-01 23:57:12 +0800

    deploy

--- assets/modalMixin-dbb0e79f.js
+++
@@ -1,1 +0,0 @@
-import{M as o}from"./modal-8c1c62b8.js";const d={data(){return{modal:{}}},methods:{showModal(){this.modal.show()},hideModal(){this.modal.hide()}},mounted(){this.modal=new o(this.$refs.modal,{keyboard:!1,backdrop:"static"})}};export{d as m};
