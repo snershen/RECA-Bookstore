5de96d4d44351b94e4e2a7afa8f2ca918969f54d
Wu Jane <jane72418@gmail.com>
2024-02-24 23:32:32 +0800

    deploy

---
+++ assets/alertMixin-b9e13f4c.js
@@ -0,0 +1,1 @@
+import{S as e}from"./sweetalert2.all-03d2c133.js";const r={methods:{showAlert(t){return e.fire({confirmButtonText:"確定",cancelButtonText:"取消",...t})}}};export{r as a};
--- assets/alertMixin-c5f7ae65.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./sweetalert2.all-1c7eb4a3.js";const r={methods:{showAlert(t){return e.fire({confirmButtonText:"確定",cancelButtonText:"取消",...t})}}};export{r as a};
