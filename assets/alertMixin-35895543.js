406ae58ce19dccd9c103d70118f8ee0eccb09d3c
Wu Jane <jane72418@gmail.com>
2024-03-21 22:13:38 +0800

    deploy

--- assets/alertMixin-35895543.js
+++
@@ -1,1 +0,0 @@
-import{S as o}from"./toastMixin-b579257b.js";const n={methods:{showAlert(t){return o.fire({confirmButtonText:"確定",cancelButtonText:"取消",confirmButtonColor:"#000",...t})}}},e=t=>o.fire({confirmButtonText:"確定",cancelButtonText:"取消",confirmButtonColor:"#000",...t});export{n as a,e as s};
